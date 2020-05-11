fe9b9e2873bd0761a43f92ac1d7ed91d47bd0999
LOKESH GOUNDER <lokeshgounder@gmail.com>
2020-05-11 15:40:02 +0530

    client

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '4lfdm7kbx9'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/three`
+
+export const authConfig = {
+  domain: 'lokeshgounder.auth0.com',
+  clientId: 'otIb4e2yjpJ8IG839U3q18maktBCs6FP',
+  callbackUrl: 'http://localhost:3000/callback'
+}
